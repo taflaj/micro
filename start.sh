4903ad3978cb5f77bf35ac4addca25c76dbb9d5f
José Tafla <taflaj@users.noreply.github.com>
2021-12-10 15:39:04 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,4 +9,4 @@ sleep 1
 # go run github.com/taflaj/micro/pubkey pubkey.db |& tee -a pubkey.log &
 go run github.com/taflaj/micro/random |& tee -a random.log &
 sleep 1
-go run github.com/taflaj/micro/server ad18d93d20f79d |& tee -a server.log &
+go run github.com/taflaj/micro/server |& tee -a server.log &
